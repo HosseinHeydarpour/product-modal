0900e0a5fc2cf17ae852c0dc99698d06f9765f13
HosseinHeydarpour <hosseinheydarpour1@gmail.com>
2025-04-04 12:30:33 +0330

    feat: add outline btn component to the project

---
+++ src/app/outline-btn/outline-btn.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-outline-btn',
+  standalone: true,
+  imports: [],
+  templateUrl: './outline-btn.component.html',
+  styleUrl: './outline-btn.component.scss'
+})
+export class OutlineBtnComponent {
+
+}
