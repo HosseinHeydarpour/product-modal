cb89eb7146079fdb91b7c31d8cea6a1b7217c03a
HosseinHeydarpour <hosseinheydarpour1@gmail.com>
2025-04-04 13:11:34 +0330

    feat: add fallback state to the component

--- src/app/shopping-card/shopping-card.component.ts
+++ src/app/shopping-card/shopping-card.component.ts
@@ -3,24 +3,26 @@ import { PrimaryBtnComponent } from '../primary-btn/primary-btn.component';
 
 import { ProductService } from '../product.service';
 import { Product } from '../product.model';
+import { FakepricePipe } from '../fakeprice.pipe';
 
 const RANDOM_ID = Math.floor(Math.random() * 10);
 
 @Component({
   selector: 'app-shopping-card',
   standalone: true,
-  imports: [PrimaryBtnComponent],
+  imports: [PrimaryBtnComponent, FakepricePipe],
   templateUrl: './shopping-card.component.html',
   styleUrl: './shopping-card.component.scss',
 })
 export class ShoppingCardComponent implements OnInit {
   productService = inject(ProductService);
   product!: Product;
+  isFetching: boolean = true;
   ngOnInit(): void {
     this.productService.getProduct(RANDOM_ID).subscribe({
       next: (res) => {
         this.product = res;
-        console.log(this.product);
+        this.isFetching = false;
       },
     });
   }
